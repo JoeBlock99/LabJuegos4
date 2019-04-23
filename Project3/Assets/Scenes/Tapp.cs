b80b88393502db7fe7626a5a8c6cab13bad241f4
JoeBlock99 <40869458+JoeBlock99@users.noreply.github.com>
2019-04-23 11:52:54 -0600

    Jugeos

---
+++ Project3/Assets/Scenes/Tapp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tapp : MonoBehaviour
+{
+    public Rigidbody rb;
+    public int force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnMouseOver()
+    {
+        gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+    private void OnMouseDown()
+    {
+        Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(myRay, out hitInfo))
+        {
+            rb.AddForce(-hitInfo.normal * force, ForceMode.Impulse);
+        }
+    }
+}
